fa971a215124641929c14ef372dd3ef90f172d17
tmanas06 <2210030003@klh.edu.in>
2026-01-12 21:42:42 +0530

    Update hardhat config for Hardhat 2.x compatibility

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,7 +17,6 @@ export default {
   networks: {
     mantleSepolia: {
       url: process.env.MANTLE_SEPOLIA_RPC || "https://rpc.sepolia.mantle.xyz",
-      type: "http",
       chainId: 5003,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
